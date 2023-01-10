7aa81cf1bec3496fcb17ba6db92c3811bd3775f3
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-01-10 23:14:50 +0600

    Update

--- src/js/edit-user.js
+++ src/js/edit-user.js
@@ -3,5 +3,31 @@
 import { handleForm } from './utils/form';
 
 handleForm('edit-user');
-
 console.log('edit-user page');
+
+const updateAvatarButton = document.querySelector('.avatar-overlay');
+const updateAvatarModal = document.querySelector('.change-avatar-modal');
+const updateAvatarModalCloseButton = document.querySelector('.change-avatar-modal__close');
+
+function handleModal(val) {
+  updateAvatarModal.hidden = val;
+}
+
+
+updateAvatarButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  handleModal(false);
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      handleModal(true);
+      window.removeEventListener('keydown');
+    }
+  });
+});
+
+updateAvatarModalCloseButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  handleModal(true);
+  window.removeEventListener('keydown');
+});
+
