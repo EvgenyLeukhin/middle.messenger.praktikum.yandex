9821923e050c832aad661abaee0eab22f052606e
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-01-28 07:16:36 +0600

    Update

---
+++ src/routes/index.ts
@@ -0,0 +1,21 @@
+import Authorization from '../routes/Authorization';
+import ChangePassword from '../routes/ChangePassword';
+import Chat from '../routes/Chat';
+import EditUser from '../routes/EditUser';
+import Page404 from '../routes/Page404';
+import Page500 from '../routes/Page500';
+import Registration from '../routes/Registration';
+import TestPage from '../routes/TestPage';
+import UserSettings from '../routes/UserSettings';
+
+export {
+  Authorization,
+  ChangePassword,
+  Chat,
+  EditUser,
+  Page404,
+  Page500,
+  Registration,
+  TestPage,
+  UserSettings,
+}
