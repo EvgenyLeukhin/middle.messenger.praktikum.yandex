f085d34554f7541fd3c03e6db6176e3c701707ee
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-01-28 14:42:53 +0600

    Update

--- src/spa/render.ts
+++ src/spa/render.ts
@@ -2,8 +2,10 @@ import Component from './Component';
 
 function render(query: string, component: Component) {
   const root = document.querySelector(query) as HTMLElement;
+  // add element to Virtual DOM
   root.appendChild(component.getElement());
 
+  // add components lifecycle handler
   component.dispatchMountComponent();
 }
 
