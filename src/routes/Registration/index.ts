e44153cee9bfb03cccc77317399c039274c287b6
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-01-29 13:04:00 +0600

    Registration template

--- src/routes/Registration/index.ts
+++ src/routes/Registration/index.ts
@@ -1,7 +1,33 @@
-import Component from '../../utils/spa/Component';
 import template from './template';
+import Component from '../../utils/spa/Component';
+import { FormLoader } from '../../components';
 
 class Registration extends Component {
+  constructor(props: Record<string, any> = {}) {
+    const formLoader = new FormLoader(props);
+    const registrationEmailInput = null
+    const registrationLoginlInput = null
+    const registrationNameInput = null
+    const registrationSecondNameInput = null
+    const registrationPhoneInput = null
+    const registrationPasswordInput = null
+    const registrationPasswordRepeatInput = null
+    const registrationSubmitButton = null
+
+    super('div', {
+      formLoader,
+      registrationEmailInput,
+      registrationLoginlInput,
+      registrationNameInput,
+      registrationSecondNameInput,
+      registrationPhoneInput,
+      registrationPasswordInput,
+      registrationPasswordRepeatInput,
+      registrationSubmitButton,
+      ...props,
+    });
+  }
+
   render() {
     return this.setTemplate(template, this.props);
   }
--- src/routes/Registration/template.ts
+++ src/routes/Registration/template.ts
@@ -1,9 +1,30 @@
-import ejs from 'ejs';
+import * as ejs from 'ejs';
 
 const string = `
-  <section class="registration">
-    <h1>Registration</h1>
-  </section>
+  <main>
+    <section class="registration">
+      <div class="container">
+        <h2>Регистрация</h2>
+
+        <form id="registration" class="registration__form" action="" method="post">
+          <%- formLoader %>
+
+          <%- registrationEmailInput %>
+          <%- registrationLoginlInput %>
+          <%- registrationNameInput %>
+          <%- registrationSecondNameInput %>
+          <%- registrationPhoneInput %>
+          <%- registrationPasswordInput %>
+          <%- registrationPasswordRepeatInput %>
+
+          <div class="form-controls">
+            <%- registrationSubmitButton %>
+            <a href="authorization.ejs">Войти</a>
+          </div>
+        </form>
+      </div>
+    </section>
+  </main>
 `;
 
 const template = ejs.compile(string);
