a896151237eac42e29884a564972707cd36ff135
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-01-30 17:24:00 +0600

    Add change-password page

--- src/routes/ChangePassword/index.ts
+++ src/routes/ChangePassword/index.ts
@@ -1,7 +1,61 @@
+import { Button, FormInput, FormLoader, UserAvatar } from '../../components';
 import Component from '../../utils/spa/Component';
 import template from './template';
 
 class ChangePassword extends Component {
+  constructor(props: Record<string, any> = {}) {
+    const userAvatar = new UserAvatar({
+      userName: 'Иван',
+      image: 'url(https://images.unsplash.com/photo-1529068755536-a5ade0dcb4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=881&q=80)',
+    });
+
+    const formLoader = new FormLoader(props);
+
+    const oldPassword = new FormInput({
+      label: 'Старый пароль',
+      id: 'change-password__oldPassword',
+      type: 'password',
+      name: 'oldPassword',
+      value: '',
+    });
+
+    const newPassword = new FormInput({
+      label: 'Новый пароль',
+      id: 'change-password__newPassword',
+      type: 'password',
+      name: 'newPassword',
+      value: '',
+    });
+
+    const newPasswordRepeat = new FormInput({
+      label: 'Повторите новый пароль',
+      id: 'change-password__newPassword-repeant',
+      type: 'password',
+      name: 'newPassword-repeant',
+      value: '',
+    });
+
+    const submitButton = new Button({
+      buttonText: 'Сохранить',
+      events: {
+        click: (e) => {
+          e.preventDefault();
+          alert('Change password form submit')
+        },
+      },
+    });
+
+    super('main', {
+      userAvatar,
+      formLoader,
+      oldPassword,
+      newPassword,
+      newPasswordRepeat,
+      submitButton,
+      ...props
+    });
+  }
+
   render() {
     return this.setTemplate(template, this.props);
   }
--- src/routes/ChangePassword/template.ts
+++ src/routes/ChangePassword/template.ts
@@ -1,9 +1,25 @@
-import ejs from 'ejs';
+import * as ejs from 'ejs';
 
 const string = `
-  <section class="change-password">
-    <h1>ChangePassword</h1>
-  </section>
+  <main>
+    <section class="ChangePassword">
+      <div class="container">
+        <%- userAvatar %>
+
+        <form class="ChangePassword__form" id="change-password" action="" method="post">
+          <%- formLoader %>
+
+          <%- oldPassword %>
+
+          <%- newPassword %>
+
+          <%- newPasswordRepeat %>
+
+          <%- submitButton %>
+        </form>
+      </div>
+    </section>
+  </main>
 `;
 
 const template = ejs.compile(string);
