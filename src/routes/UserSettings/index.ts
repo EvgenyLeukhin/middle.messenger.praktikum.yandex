84ab62a948b036dd9e0a612862b09d7ad6780205
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-01-30 10:33:15 +0600

    Update

--- src/routes/UserSettings/index.ts
+++ src/routes/UserSettings/index.ts
@@ -1,10 +1,31 @@
-import Component from '../../utils/spa/Component';
 import template from './template';
+import Component from '../../utils/spa/Component';
 
-class UserSettings extends Component {
+class Page500 extends Component {
+  constructor(props: Record<string, any> = {}) {
+    const userAvatar = null;
+    const userEmail = 'pochta@yandex.ru';
+    const userLogin = 'ivanivanov';
+    const userName = 'Иван';
+    const userSecondName = 'Иванов';
+    const userNickname = 'Иван';
+    const userPhone = '+7 (909) 967 30 30';
+
+    super('main', {
+      userAvatar,
+      userEmail,
+      userLogin,
+      userName,
+      userSecondName,
+      userNickname,
+      userPhone,
+      ...props
+    });
+  }
+
   render() {
     return this.setTemplate(template, this.props);
   }
 }
 
-export default UserSettings;
+export default Page500;
--- src/routes/UserSettings/template.ts
+++ src/routes/UserSettings/template.ts
@@ -1,9 +1,51 @@
-import ejs from 'ejs';
+import * as ejs from 'ejs';
 
 const string = `
-  <section class="user-settings">
-    <h1>Authorization</h1>
-  </section>
+  <main>
+    <section class="user-settings">
+      <div class="container">
+
+        <%- userAvatar %>
+
+        <div class="user-settings__middle">
+          <ul class="user-settings__data-list">
+            <li>
+              <b>Почта</b>
+              <span><%- userEmail %></span>
+            </li>
+            <li>
+              <b>Логин</b>
+              <span><%- userLogin %></span>
+            </li>
+            <li>
+              <b>Имя</b>
+              <span><%- userName %></span>
+            </li>
+            <li>
+              <b>Фамилия</b>
+              <span><%- userSecondName %></span>
+            </li>
+            <li>
+              <b>Имя в чате</b>
+              <span><%- userNickname %></span>
+            </li>
+            <li>
+              <b>Телефон</b>
+              <span><%- userPhone %></span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="user-settings__bottom">
+          <ul class="user-settings__controls-list">
+            <li><a href="/edit-user" class="text-primary">Изменить данные</a></li>
+            <li><a href="/change-password" class="text-primary">Изменить пароль</a></li>
+            <li><a href="/authorization" class="text-error">Выйти</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </main>
 `;
 
 const template = ejs.compile(string);
